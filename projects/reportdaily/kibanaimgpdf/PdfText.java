661b5d3eb3dfb2b4a663931114be339dabbe51d4
Fabio Vecchionacce <32280495+Fabione2005@users.noreply.github.com>
2025-10-26 17:46:21 -0300

    Delete projects/reportdaily/kibanaimgpdf/PdfText.java

--- projects/reportdaily/kibanaimgpdf/PdfText.java
+++
@@ -1,9 +0,0 @@
-final class PdfText {
-  static String tryExtractQuick(Path pdf) throws IOException {
-    try (PDDocument doc = Loader.loadPDF(pdf.toFile())) {
-      PDFTextStripper stripper = new PDFTextStripper();
-      stripper.setSortByPosition(true);
-      return stripper.getText(doc);
-    }
-  }
-}
