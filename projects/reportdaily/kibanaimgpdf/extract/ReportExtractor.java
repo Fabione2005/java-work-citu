6bdf90ccfd19a8b1a35aa15fc9d096dec372dc1f
Fabio Vecchionacce <32280495+Fabione2005@users.noreply.github.com>
2025-10-28 16:37:59 -0300

    Delete projects/reportdaily/kibanaimgpdf/extract directory

--- projects/reportdaily/kibanaimgpdf/extract/ReportExtractor.java
+++
@@ -1,9 +0,0 @@
-
-package com.tuempresa.reports.extract;
-
-import java.io.File;
-import java.util.Map;
-
-public interface ReportExtractor {
-    Map<String, Long> extract(File pdf) throws Exception;
-}
